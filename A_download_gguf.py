ad9a9b2878d0a279fc3b36ad2daf88f117c0c942
Yingying010 <wangyingying200101@163.com>
2025-07-17 19:39:49 +0100

    update

---
+++ A_download_gguf.py
@@ -0,0 +1,10 @@
+from huggingface_hub import hf_hub_download
+
+gguf_path = hf_hub_download(
+    repo_id="YingyingWang/Qwen3_single_move_turn_gguf",
+    filename="Qwen3_single_move_turn_q4_k_m.gguf",  # 替换成你模型的文件名
+    local_dir="models",  # 指定下载目录
+    local_dir_use_symlinks=False  # 避免使用符号链接，强制复制文件
+)
+
+print("✅ 下载完成，路径为：", gguf_path)
