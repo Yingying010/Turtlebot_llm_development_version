92a3f16af708384b80e6bedf59cb01d7a12408d9
Yingying010 <wangyingying200101@163.com>
2025-07-18 12:18:33 +0100

    add play audio and beep

--- play.py
+++ play.py
@@ -2,7 +2,7 @@ import subprocess
 
 def play_beep_aplay(path):
     try:
-        subprocess.run(["aplay", "-D", "plughw:2,0"], check=True)
+        subprocess.run(["aplay", "-D", "plughw:2,0",path], check=True)
     except Exception as e:
         print(f"播放失败: {e}")
 
