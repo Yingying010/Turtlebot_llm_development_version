aaac29d09440bdcf6edf428c82d9551272733e49
Yingying010 <wangyingying200101@163.com>
2025-07-21 18:28:16 +0100

    change the full model

--- download_model.py
+++ download_model.py
@@ -1,9 +1,9 @@
 from huggingface_hub import hf_hub_download
 
 gguf_path = hf_hub_download(
-    repo_id="YingyingWang/Qwen3_base_instruction_q4_k_m",
-    filename="Qwen3_base_instruction_q4_k_m.gguf",  # 替换成你模型的文件名
-    local_dir="models",  # 指定下载目录
+    repo_id="YingyingWang/Qwen3_base_instructionk_m",
+    filename="Qwen3_base_instruction",  # 替换成你模型的文件名
+    local_dir="models/Qwen3_base_instruction",  # 指定下载目录
     local_dir_use_symlinks=False  # 避免使用符号链接，强制复制文件
 )
 
