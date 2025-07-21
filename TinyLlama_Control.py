0c1cc9db6ebb44365cdeae667177f99e233c94cb
Yingying010 <wangyingying200101@163.com>
2025-07-21 17:58:25 +0100

    test turn

--- TinyLlama_Control.py
+++ TinyLlama_Control.py
@@ -95,7 +95,7 @@ def run(user_input: str):
 
 # ===== 测试入口 =====
 if __name__ == "__main__":
-    user_input = "let robot 1 turn left 45 degrees"
+    user_input = "let robot 1 turn right 45 degrees"
     response = run(user_input)
     if response:
         control_turtlebot.run(response)
