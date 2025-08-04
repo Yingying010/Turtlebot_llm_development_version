61c53261c2d70712f7854b3cdccd2cf1fa41985c
Yingying010 <wangyingying200101@163.com>
2025-08-04 15:43:44 -0400

    correct the path of tts

--- ttsRepo/stream_tts_test.py
+++ ttsRepo/stream_tts_test.py
@@ -1,8 +1,4 @@
 import os
-from ttsRepo.stream_tts import tts_manager
+from stream_tts import tts_manager
 
-# 设置远程 PulseAudio 服务器地址（树莓派 IP）
-# os.environ["PULSE_SERVER"] = "tcp:192.168.0.208"  # ⚠️ 记得替换成你的 IP，例如 192.168.1.42
-
-# 测试播报
 tts_manager.say("Hello! This is a test")
