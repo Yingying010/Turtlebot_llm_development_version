1052a837722d963441eb8df894905365602e28ca
Yingying010 <wangyingying200101@163.com>
2025-08-09 23:24:21 +0800

    Modified the recognition logic for listening to voices, deleted run and tried it.

--- ai_assistant.py
+++ ai_assistant.py
@@ -3,7 +3,7 @@ import os
 PROJECT_ROOT = os.path.dirname(os.path.dirname(__file__))
 sys.path.append(PROJECT_ROOT)
 from config import config
-from WhisperRepo.whisper_recognizer import Whisper_run, conversation_active, recognize
+from WhisperRepo.whisper_recognizer import conversation_active, recognize
 from llmParserRepo.qwen3_parser import run_conversation_loop
 # import TinyLlama_Chat
 import time
