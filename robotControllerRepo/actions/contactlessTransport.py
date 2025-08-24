a0c60047409b4fcc8278ff45dd8838a6f8c919cf
Yingying010 <wangyingying200101@163.com>
2025-08-24 17:06:30 +0800

    debugging

--- robotControllerRepo/actions/contactlessTransport.py
+++ robotControllerRepo/actions/contactlessTransport.py
@@ -38,7 +38,7 @@ from robotControllerRepo.actions.navigate import navigate_to_target, get_current
 # ===== 全局参数（直接改这里就行） =====
 PARTICLE = (0.0, -500.0)     # 微粒点坐标
 TARGET   = (500.0, -1000.0)    # 目标点坐标
-GAP = 80 
+GAP = 70 
 LENGTH = 138
 WIDTH = 178
 SPEED    = 0.02
@@ -54,7 +54,7 @@ def plan_formation(particle_xy, target_xy):
     px, py = particle_xy
     tx, ty = target_xy
 
-    phi = -math.atan2(ty - py, tx - px)
+    phi = math.atan2(ty - py, tx - px)
 
     r1_x = GAP + LENGTH/2 - px
     r1_y = py  - (GAP + WIDTH/2)
