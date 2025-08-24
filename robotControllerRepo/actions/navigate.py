8a56f9faee6c003e59cc7f2a779ac9b44372d1c3
Yingying010 <wangyingying200101@163.com>
2025-08-24 21:00:35 +0800

    test rotate in navi and modifyduration_sec

--- robotControllerRepo/actions/navigate.py
+++ robotControllerRepo/actions/navigate.py
@@ -199,8 +199,16 @@ def precision_rotate(node: Node, robot_name: str, target_angle_deg: float,
         print(f"[PRECISION_ROTATE] Already aligned: current={current_heading:.1f}deg, "
               f"target={target_angle_deg:.1f}deg, error={angle_diff:.1f}deg")
         return
+    
+    r1_error = 10
+    r2_error = 0
+
     if robot_name == "robot1":
-        duration_sec = abs(angle_diff)+10 / angular_speed_deg_per_s
+        duration_sec = abs(angle_diff)+r1_error / angular_speed_deg_per_s
+
+    if robot_name == "robot2":
+        duration_sec = abs(angle_diff)+r2_error / angular_speed_deg_per_s
+    
     angular_speed_rad_per_s = math.radians(angular_speed_deg_per_s)
     
     twist = Twist()
