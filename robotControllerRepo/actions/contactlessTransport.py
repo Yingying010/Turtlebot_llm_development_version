d30664883b9580a457d365b30b004242d3f1bac8
Yingying010 <wangyingying200101@163.com>
2025-08-24 17:16:49 +0800

    debugging

--- robotControllerRepo/actions/contactlessTransport.py
+++ robotControllerRepo/actions/contactlessTransport.py
@@ -38,9 +38,9 @@ from robotControllerRepo.actions.navigate import navigate_to_target, get_current
 # ===== 全局参数（直接改这里就行） =====
 PARTICLE = (0.0, -500.0)     # 微粒点坐标
 TARGET   = (500.0, -1000.0)    # 目标点坐标
-GAP = 73 
+GAP = 76 
 LENGTH = 138
-WIDTH = 170
+WIDTH = 168
 SPEED    = 0.02
 
 # 超时配置（秒）
