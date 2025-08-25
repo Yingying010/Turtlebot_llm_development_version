c1b6a32b564f30c8c9cd3ea93a03bdf1acfbd1ce
Yingying010 <wangyingying200101@163.com>
2025-08-25 17:05:48 +0800

    change return

--- robotControllerRepo/actions/dropoff.py
+++ robotControllerRepo/actions/dropoff.py
@@ -4,10 +4,9 @@ from ttsRepo.stream_tts import tts_manager
 
 
 def dropoff_item(item: str):
-    is_successful = False
     print(f"🗣️ Speaking: I am dropping off {item}")
     tts_manager.say(f"I am dropping off {item}")
     time.sleep(3)  # 模拟收集时间
     tts_manager.say(f"{item} dropped off successfully")
 
-    return is_successful
+    return True
--- robotControllerRepo/actions/pickup.py
+++ robotControllerRepo/actions/pickup.py
@@ -4,10 +4,9 @@ from ttsRepo.stream_tts import tts_manager
 
 
 def pickup_item(item:str):
-    is_successful = False
     print(f"🗣️ Speaking: I am picking up {item}")
     tts_manager.say(f"I am picking up {item}")
     time.sleep(3)  # 模拟收集时间
     tts_manager.say(f"{item} picked up successfully")
 
-    return is_successful
+    return True
